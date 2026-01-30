d7eeb52d990dfeb2f5387b8875fe3cfa6660aedb
Adobiz <3161767696@qq.com>
2026-01-31 01:37:03 +0800

    菜单完成

---
+++ Assets/Scripts/Menu/QuitButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{ 
+    public void QuitGame(){
+        print("成功");
+        Application.Quit();
+    }
+}
